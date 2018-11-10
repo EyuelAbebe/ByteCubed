9eef212e0ef71edd0ab108686fe81b657eaec5f7
eyueld <eyueld@gmail.com>
2018-11-10 05:15:15 -0500

    Added more condition to check if sitting guests also dislikes incoming guest. Updated location of table information change.

--- src/com/bytecubed/WeddingSeating.java
+++ src/com/bytecubed/WeddingSeating.java
@@ -1,14 +1,16 @@
 package com.bytecubed;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class WeddingSeating {
 
     private static List<Map> tables;
     private static List<Map> guestInfos;
+
+    // Used to record list of moved guests from table.
+    private static List<String> movedGuests = new ArrayList<>();
 
     public WeddingSeating(List tables, List guestInfos) {
         this.tables = tables;
@@ -34,7 +36,9 @@ public class WeddingSeating {
      * @param guest guest info.
      * @return boolean.
      */
-    public boolean seatGuest(Map guest){
+    private boolean seatGuest(Map guest){
+
+        movedGuests.clear();
 
         for(Map table: tables){
 
@@ -53,21 +57,35 @@ public class WeddingSeating {
             int guestSize = (int) guest.get("size");
             List<String> seating = (List) table.get("seating");
 
+            if (guestSize > (int) table.get("size")){
+                continue;
+            }
+
             for (String sittingGuest: seating){
                 Map sittngGuestInfo = guestInfos.stream()
                         .filter( guestInfo -> guestInfo.get("name").toString().trim().equalsIgnoreCase(sittingGuest))
                         .findAny()
                         .orElse(null);
 
+                if (canMove(sittngGuestInfo, table)) {
+                    int tableSpaceAvailable = (int) table.get("spaceAvailable");
+                    int sittingGuestSize = (int) sittngGuestInfo.get("size");
 
-                if (canMove(sittngGuestInfo, table)){
-                    seating.add(guest.get("name").toString().trim().toUpperCase());
-                    int spaceAvailable = (int) table.get("spaceAvailable");
-                    table.replace("spaceAvailable", spaceAvailable - guestSize);
-                    return true;
+                    table.replace("spaceAvailable", tableSpaceAvailable + sittingGuestSize);
+
+                    if (guestSize <= (int) table.get("spaceAvailable")) {
+                        seating.add(guest.get("name").toString().trim().toUpperCase());
+                        int spaceAvailable = (int) table.get("spaceAvailable");
+                        table.replace("spaceAvailable", spaceAvailable - guestSize);
+                        ((List) table.get("seating")).removeAll(movedGuests);
+                        return true;
+                    }
                 }
             }
+            ((List) table.get("seating")).removeAll(movedGuests);
         }
+
+
         return false;
     }
 
@@ -79,17 +97,26 @@ public class WeddingSeating {
      * @param seating list of people sitting on table.
      * @return boolean.
      */
-    public boolean canSeat(Map guestInfo, int tableSizeAvailable, List<String> seating){
+    private boolean canSeat(Map guestInfo, int tableSizeAvailable, List<String> seating){
 
         if ( tableSizeAvailable <  (int) guestInfo.get("size") ){
             return false;
         }
 
+        if (seating.isEmpty()){return true;}
+
         if (guestInfo.get("dislikes") != null){
             for (String dislike: (List<String>) guestInfo.get("dislikes")) {
-                if (seating.stream().anyMatch(s -> s.equals(dislike))) {return false;};
+                if (seating.stream().anyMatch(s -> s.equalsIgnoreCase(dislike))) {return false;};
             }
         }
+
+        for (String sitting: seating){
+            if (dislikes(sitting, guestInfo.get("name").toString())){
+                return false;
+            }
+        }
+
         return true;
     }
 
@@ -100,7 +127,7 @@ public class WeddingSeating {
      * @param srcTable table name guest currently sitting on.
      * @return boolean
      */
-    public boolean canMove(Map guestInfo, Map srcTable){
+    private boolean canMove(Map guestInfo, Map srcTable){
 
         for(Map destTable : tables){
             if ( destTable.get("name") != srcTable.get("name")){
@@ -115,24 +142,41 @@ public class WeddingSeating {
     }
 
     /**
-     * Moves a guest from srcTable to destinationTable. Updates tables info accordingly.
+     * Moves a guest from srcTable to destinationTable. Updates destination table info accordingly.
      *
      * @param guestInfo
      * @param srcTable
      * @param destinationTable
      */
-    public void move(Map guestInfo, Map srcTable, Map destinationTable){
+    private void move(Map guestInfo, Map srcTable, Map destinationTable){
 
         int guestSize = (int) guestInfo.get("size");
-        srcTable.replace("spaceAvailable", (int) srcTable.get("spaceAvailable") + guestSize);
         destinationTable.replace("spaceAvailable", (int) destinationTable.get("spaceAvailable") - guestSize);
 
+        List<String> destTableSeating = (List) destinationTable.get("seating");
+        destTableSeating.add(guestInfo.get("name").toString().trim().toUpperCase());
 
-        List<String> guestTableSeating = (List) destinationTable.get("seating");
-        guestTableSeating.add(guestInfo.get("name").toString().trim().toUpperCase());
+        movedGuests.add(guestInfo.get("name").toString().trim().toUpperCase());
+    }
 
-        List<String> srcTableSeating = (List) srcTable.get("seating");
-        srcTableSeating.remove(guestInfo.get("name").toString().trim().toUpperCase());
 
+    /**
+     * Checks if sittingGuest dislikes incomingGuest
+     *
+     * @param sittingGuest name of sitting guest
+     * @param incomingGuestName name of guest looking to sit
+     * @return boolean
+     */
+    private boolean dislikes(String sittingGuest, String incomingGuestName){
+
+        Map sittngGuestInfo = guestInfos.stream()
+                .filter( guestInfo -> guestInfo.get("name").toString().trim().equalsIgnoreCase(sittingGuest))
+                .findAny()
+                .orElse(null);
+
+        if (sittngGuestInfo.get("dislikes") != null){
+            return ((List<String>)sittngGuestInfo.get("dislikes")).contains(incomingGuestName.trim().toUpperCase());
+        }
+        return false;
     }
 }
